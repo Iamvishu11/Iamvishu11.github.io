d61297afeaf3f5cd1e7d58fcd9bd3d6aeefdfb3a
Vishal Kumar Singh <30724582+Iamvishu11@users.noreply.github.com>
2018-04-28 04:16:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,114 @@
+var config = {
+    apiKey: "AIzaSyBbgk4LzR7rJY43ka817sS3V-aJ0rFJ9l0",
+    authDomain: "email-form-11.firebaseapp.com",
+    databaseURL: "https://email-form-11.firebaseio.com",
+    projectId: "email-form-11",
+    storageBucket: "email-form-11.appspot.com",
+    messagingSenderId: "463160700980"
+};
+firebase.initializeApp(config);
+var users;
+var exists =false;
+$(function () {
+
+
+//   //Reference username collection
+    var userRef = firebase.database().ref('username');
+    firebase.database().ref('username').on('value', function (snapshot) {
+        users = snapshot.val();
+
+    }, function (err) {
+        console.log(err);
+    });
+
+
+ //get values
+ var email = $("#Email").val();
+
+//listen for form submit
+    document.getElementById('email-form').addEventListener('submit',submitForm);
+
+    function checkIfUserExists(userId) {
+
+
+        var arr = Object.values(users);
+
+        arr.forEach( function (arrayItem)
+        {
+            if(arrayItem.user === userId){
+                
+              console.log(userId);
+              exists=true;
+            }
+        });
+        
+         userExistsCallback(exists);
+
+
+    }
+
+    function userExistsCallback(exists) {
+        if (exists) {
+              //show alert
+            document.querySelector('.alert2').style.display ='block';
+
+            //Hide alert after 5 seconds
+            setTimeout(function(){
+                document.querySelector('.alert2').style.display ='none';
+                location.reload(true);
+            },3000);
+           
+
+        } else {
+
+            //save message
+            saveUser(email);
+
+            //show alert
+            document.querySelector('.alert').style.display ='block';
+
+            //Hide alert after 5 seconds
+            setTimeout(function(){
+                document.querySelector('.alert').style.display ='none';
+                location.reload(true);
+            },3000);
+
+        }
+    }
+
+
+
+
+
+
+    //fun to get form values
+    function getInputVal(id){
+        return document.getElementById(id).value;
+    }
+    //save message to firebase
+    function saveUser(email){
+
+        var newUser = userRef.push();
+        newUser.set({
+            user: email
+        })
+    }
+
+
+//Submit form
+    function submitForm(e){
+        e.preventDefault();
+
+       //get values
+       email = $("#Email").val();
+
+        //check if user exist
+
+        checkIfUserExists(email);
+
+
+    }
+
+
+
+});
